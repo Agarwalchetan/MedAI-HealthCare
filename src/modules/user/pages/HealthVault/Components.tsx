0add6a7762634aa837223fd05b823b59d4f6e9a6
Aditya Kumar Maurya <adityamaurya1947@gmail.com>
2025-09-28 21:52:41 +0530

    changed the gemini model to gemini-2.5-flash from gemini-1.5-flash because gemini-1.5-flash is deprecated

--- src/modules/user/pages/HealthVault/Components.tsx
+++ src/modules/user/pages/HealthVault/Components.tsx
@@ -103,7 +103,7 @@ export const FileUploadModal: React.FC<FileUploadModalProps> = ({ isOpen, onClos
         try {
             //Gemini AI
             const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY || '');
-            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+            const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
             //file to base64 conversion (we can upload the direct image to gemini ai but fist we have to upload it any where else and then update it)
             //so, converting to base64 is an easy and time efficent option for both ai and user
